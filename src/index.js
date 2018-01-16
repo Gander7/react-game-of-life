a7b38d1e79eebe28dba6da7b2827b4fc7916db93
Zach Allen <zachjallen@gmail.com>
2018-01-16 13:55:19 -0400

    added Main class

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Main extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Game Of Life</h1>
+        <Grid
+        />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<p>Hello World</p>, document.getElementById('root'));
